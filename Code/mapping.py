3d6eb6a8d02f1ab02df899b46f5e7087d5099e54
Tim McWilliams <tmcwilliams@smu.edu>
2018-05-01 22:29:40 -0400

    Added try/except & print statments

--- Code/mapping.py
+++ Code/mapping.py
@@ -21,22 +21,30 @@ gbq.to_gbq(mt_Gox_Hack, 'DB.1LNWw6yCxkUmkhArb2Nf2MPw6vG7u5WG7q', projectid, if_e
 # 2. query for the history for that pubKey,
 # 3. save that query as a table in our DB 
 for g in mt_Gox_Hack['outputs_output_pubkey_base58']:
-    gox_pubKeys = gbq.read_gbq('SELECT timestamp, transaction_id, inputs.input_pubkey_base58, outputs.output_satoshis, outputs.output_pubkey_base58 FROM (FLATTEN(DB.transactions_all, outputs.output_satoshis)) WHERE inputs.input_pubkey_base58 == "{}"'.format(g), projectid)
-    gbq.to_gbq(gox_pubKeys, 'DB.{}'.format(g), projectid, if_exists='replace')
-
+    try:
+        gox_pubKeys = gbq.read_gbq('SELECT timestamp, transaction_id, inputs.input_pubkey_base58, outputs.output_satoshis, outputs.output_pubkey_base58 FROM (FLATTEN(DB.transactions_all, outputs.output_satoshis)) WHERE inputs.input_pubkey_base58 == "{}"'.format(g), projectid)
+        gbq.to_gbq(gox_pubKeys, 'DB.{}'.format(g), projectid, if_exists='replace')
+    except KeyError, e:
+        print("Error! type:{} - message:{}".format(str(type(e)), str(e)))
+    
+    print(g + " The Gox PubKeys are Mapped!!")
+    
 ## Creating transaction history per pubKey (address)
 # Get transaction table form BigQuery
-transaction = gbq.read_gbq('SELECT timestamp, transaction_id, inputs.input_pubkey_base58, outputs.output_satoshis, outputs.output_pubkey_base58 FROM (FLATTEN(DB.transactions_all, outputs.output_satoshis)) LIMIT 1250000', projectid)
+transaction = gbq.read_gbq('SELECT timestamp, transaction_id, inputs.input_pubkey_base58, outputs.output_satoshis, outputs.output_pubkey_base58 FROM (FLATTEN(DB.transactions_all, outputs.output_satoshis)) WHERE inputs.input_pubkey_base58 IS NOT NULL LIMIT 1250000', projectid)
 
 # Map the transaction history of all pubKeys 
 # 1. loop through each pubKey in transaction table, 
 # 2. query for the history for that pubKey,
 # 3. save that query as a table in our DB 
 for x in transaction['inputs_input_pubkey_base58']:
-    pubKey = gbq.read_gbq('SELECT timestamp, transaction_id, inputs.input_pubkey_base58, outputs.output_satoshis, outputs.output_pubkey_base58 FROM (FLATTEN(DB.transactions_all, outputs.output_satoshis)) WHERE inputs.input_pubkey_base58 == "{}"'.format(x), projectid)
-    gbq.to_gbq(pubKey, 'DB.{}'.format(x), projectid, if_exists='replace')
-
-    print(x)
+    try:
+        pubKey = gbq.read_gbq('SELECT timestamp, transaction_id, inputs.input_pubkey_base58, outputs.output_satoshis, outputs.output_pubkey_base58 FROM (FLATTEN(DB.transactions_all, outputs.output_satoshis)) WHERE inputs.input_pubkey_base58 == "{}"'.format(x), projectid)
+        gbq.to_gbq(pubKey, 'DB.{}'.format(x), projectid, if_exists='replace')
+    except KeyError, e:
+        print("Error! type:{} - message:{}".format(str(type(e)), str(e)))
+        
+    print(x + " Mapped!!")
 
 
 
